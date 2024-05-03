3bd5b09bee14e66a8600bca92ede11787d98bd9f
Supriya Roy <supriyaroy.982000@gmail.com>
2024-05-03 20:05:28 +0530

    Updated folder

--- Processing/src/main/java/TryProcessing.java
+++
@@ -1,8 +0,0 @@
-import processing.core.PApplet;
-
-public class TryProcessing extends PApplet {
-
-    public static void main(String[] args) {
-        PApplet.main("TryProcessing",args);
-    }
-}
